093fba74a869f46e0c65c8cba0ac4d520db46289
Tasuku Suzuki <stasuku@gmail.com>
2024-01-15 10:51:02 +0900

    Use ch32v00x_hal

--- build.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    // Tell rustc to pass linker scripts to LLD
-    println!("cargo:rustc-link-arg=-Tmemory.x");
-    println!("cargo:rustc-link-arg=-Tlink.x");
-
-    // Rerun this script only when necesary
-    println!("cargo:rerun-if-changed=memory.x");
-    println!("cargo:rerun-if-changed=build.rs");
-}
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,25 @@
 #![no_std]
 #![no_main]
 
-use riscv_rt::entry;
+use ch32v_rt::entry;
+use ch32v00x_hal::prelude::*;
+use ch32v00x_hal::pac::Peripherals;
 use panic_halt as _;
 
 #[entry]
 fn main() -> ! {
-    let RCC_APB2PCENR: *mut u32 = 0x4002_1018 as _;
-    let GPIOC_CFGLR: *mut u32 = 0x4001_1000 as _;
-    let GPIOC_OUTDR: *mut u32 = 0x4001_100C as _;
+    let pac = Peripherals::take().unwrap();
 
-    unsafe {
-        // Enable clocks to the GPIOC bank
-        RCC_APB2PCENR.write_volatile(0b1_0000);
-        // Set pin 1 to output
-        GPIOC_CFGLR.write_volatile(0b0001_0000);
+    let mut rcc = pac.RCC.constrain();
+    let _clocks = rcc.config.freeze();
 
-        loop {
-            // Set pin 1 to high
-            GPIOC_OUTDR.write_volatile(0b1_0);
+    let gpioc = pac.GPIOC.split(&mut rcc);
+    let mut led = gpioc.pc1.into_push_pull_output();
+    loop {
+        led.toggle();
 
-            for _ in 0..1_000_000 {
-                core::hint::black_box(()); // Do nothing, but keep the loop
-            }
-
-            // Set pin 1 to low
-            GPIOC_OUTDR.write_volatile(0b0_0);
-
-            for _ in 0..1_000_000 {
-                core::hint::black_box(()); // Do nothing, but keep the loop
-            }
+        for _ in 0..1_000_000 {
+            core::hint::black_box(()); // Do nothing, but keep the loop
         }
     }
 }
